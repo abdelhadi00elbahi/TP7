4a5eface364d3be8f205dce060ea6ba4d17d2c6c
abdo2 <abdo2@DESKTOP-K3BQRCV>
2022-01-01 20:48:40 +0100

    TP7

---
+++ Mes TPs Java/src/ma/education/tp6/annotations/TestReflectionAnnotation.java
@@ -0,0 +1,11 @@
+package ma.education.tp6.annotations;
+
+public class TestReflectionAnnotation {
+public static void main(String[] args) {
+	Class c = CalculatriceMath.class;
+
+Programmer programmer = (Programmer)
+c.getDeclaredAnnotation(Programmer.class);
+System.out.println(programmer.id()+":"+programmer.name());
+}
+}
